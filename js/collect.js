07dd5fa051a6a1c6165eb75e74f57e9583bf246a
cjrzh <cjrzh@163.com>
2016-10-20 15:24:25 +0800

    初步完成了日志收集部分的JavaScript代码，并利用CORS技术解决了跨域请求的问题

--- js/collect.js
+++ js/collect.js
@@ -1,10 +1,4 @@
-function post(url) {
-	// body...
-	var postData = {
-		"username": "admin",
-		"action": "click",
-		"url": url
-	};
+function httpPost(jsonStr) {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
@@ -13,15 +7,50 @@ function post(url) {
 			}
 		}
 	}
-	xhr.open("POST", "localhost:8080", true);
+	console.log(jsonStr);
+	xhr.open("POST", "http://localhost:8080", true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-	xhr.send(postData)
+	xhr.send(jsonStr);
+}
+
+function postClick(url) {
+	// body...
+	var postData = {
+		"username": "admin",
+		"action": "click",
+		"url": url
+	};
+	var jsonStr = JSON.stringify(postData);
+	httpPost(jsonStr);
 
 }
 
+
+function postQuery(kw) {
+	// body...
+	var postData = {
+		"username":"张翼",
+		"action":"query",
+		"query":kw
+	};
+	var jsonStr = JSON.stringify(postData);
+	httpPost(jsonStr);
+}
+
+function toSelect() {
+	var kt = document.getElementById("keytype").value;
+	var kw = document.getElementById("keyword").value;
+	var basePath = 'http://124.133.230.227:10000/hotapp/';
+	postQuery(kw);
+	if (kw != "")
+		window.parent.document.getElementById("main1Frame").src = basePath + "system/userRecommendInfo.do?type=search&keytype=" + kt + "&keyword=" + kw;
+
+}
+
+
 function toDetail(url) {
 	//alert(url);
-	post(url);
+	postClick(url);
 	window.open(url);
 }
 toDetail('http://www.chinasafety.gov.cn/newpage/Contents/Channel_5493/2016/1019/277202/content_277202.htm');
